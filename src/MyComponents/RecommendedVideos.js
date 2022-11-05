a17e06cf6a18022f0889f0dc783fa95861f98f95
Atharva <atharvajoshi202@gmail.com>
2022-11-05 14:44:49 +0530

    Images added

--- src/MyComponents/RecommendedVideos.js
+++ src/MyComponents/RecommendedVideos.js
@@ -10,65 +10,65 @@ export default function RecommendedVideos() {
           title="| Basics of C (Part 1) | C-Programming - By W.INTELLI |"
           views="2.3M Views"
           timestamp="3 dats ago"
-          channelImage="https://yt3.ggpht.com/sMKF7G5CREq9nx5zFrLhudscdMlKE-rpybKCA8bY9FVqRmrDJNw3xGIxOsGIOD13Bey1oI3LMw=s48-c-k-c0x00ffffff-no-rj"
+          channelImage="images/1.webp"
           channel="W.INTELLI"
-          image="https://i.ytimg.com/vi/VdXGIEYZuCw/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDPgxIap5NUFaHQyDnD0_t45KXZdQ"
+          image="images/1.webp"
         />{" "}
         <VideoCard
           title="How I Trained Myself to Wake Up at 4:45am EASILY!"
           views="2.3M Views"
           timestamp="3 dats ago"
-          channelImage="https://yt3.ggpht.com/RUIA_XLTFX_TPgXYpgjYu004bldJHaQJO3cM2HNCkK0oCwRsejwV7WVeeBTIbRiW1CyYaBUuY_Y=s48-c-k-c0x00ffffff-no-rj"
+          channelImage="images/2.webp"
           channel="W.INTELLI"
-          image="https://i.ytimg.com/vi/fGCaL_YeFA4/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAPD-iq2ABx-RKQGwMoD-yTGUdszQ"
+          image="images/2.webp"
         />{" "}
         <VideoCard
           title="Option Chain Secret Trading Strategy | Regular Income from Stock Market"
           views="2.3M Views"
           timestamp="3 dats ago"
-          channelImage="https://i.ytimg.com/vi/kABCoXuhlxk/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBKHOH05r4cX7q0BJ13PGJrlmCCBQ"
+          channelImage="images/3.webp"
           channel="W.INTELLI"
-          image="https://i.ytimg.com/vi/kABCoXuhlxk/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBKHOH05r4cX7q0BJ13PGJrlmCCBQ"
+          image="images/3.webp"
         />{" "}
         <VideoCard
           title="Option Chain Secret Trading Strategy | Regular Income from Stock Market"
           views="2.3M Views"
           timestamp="3 dats ago"
-          channelImage="https://i.ytimg.com/vi/_3Zbc-O-Adw/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAOkg_dEheA_9aGoTRaqVBnU-KcJQ"
+          channelImage="images/4.webp"
           channel="W.INTELLI"
-          image="https://i.ytimg.com/vi/_3Zbc-O-Adw/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAOkg_dEheA_9aGoTRaqVBnU-KcJQ"
+          image="images/4.webp"
         />{" "}
         <VideoCard
           title="| Basics of C (Part 1) | C-Programming - By W.INTELLI |"
           views="2.3M Views"
           timestamp="3 dats ago"
-          channelImage="https://i.ytimg.com/vi/K4vXy_3ZzZ0/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCSXOcEASHqH_GsV9hAT2eFqITb-A"
+          channelImage="images/5.webp"
           channel="W.INTELLI"
-          image="https://i.ytimg.com/vi/K4vXy_3ZzZ0/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCSXOcEASHqH_GsV9hAT2eFqITb-A"
+          image="images/5.webp"
         />{" "}
         <VideoCard
           title="How I Trained Myself to Wake Up at 4:45am EASILY!"
           views="2.3M Views"
           timestamp="3 dats ago"
-          channelImage="https://i.ytimg.com/vi/MI72OdRtdzI/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBdc7if-EShv8shItAt1oeG2N3Wsg"
+          channelImage="images/6.webp"
           channel="W.INTELLI"
-          image="https://i.ytimg.com/vi/MI72OdRtdzI/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBdc7if-EShv8shItAt1oeG2N3Wsg"
+          image="images/6.webp"
         />{" "}
         <VideoCard
           title="Option Chain Secret Trading Strategy | Regular Income from Stock Market"
           views="2.3M Views"
           timestamp="3 dats ago"
-          channelImage="https://i.ytimg.com/vi/nio2-WCQZVw/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAKGLTelx6TZOeCuhHPy2BwA9simw"
+          channelImage="images/7.webp"
           channel="W.INTELLI"
-          image="https://i.ytimg.com/vi/nio2-WCQZVw/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAKGLTelx6TZOeCuhHPy2BwA9simw"
+          image="images/7.webp"
         />{" "}
         <VideoCard
           title="Option Chain Secret Trading Strategy | Regular Income from Stock Market"
           views="2.3M Views"
           timestamp="3 dats ago"
-          channelImage="https://i.ytimg.com/vi/bC7iiEaNf6I/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDw52UjVhZg61wppaBcMRddupbE_Q"
+          channelImage="images/8.webp"
           channel="W.INTELLI"
-          image="https://i.ytimg.com/vi/bC7iiEaNf6I/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDw52UjVhZg61wppaBcMRddupbE_Q"
+          image="images/8.webp"
         />{" "}
       </div>
     </div>
