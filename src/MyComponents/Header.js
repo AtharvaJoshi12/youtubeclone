066b47155388218647f2e33722cbd988883fc0e8
Atharva <atharvajoshi202@gmail.com>
2022-11-06 17:36:02 +0530

    Media queries added.

--- src/MyComponents/Header.js
+++ src/MyComponents/Header.js
@@ -7,6 +7,11 @@ import AppsIcon from "@material-ui/icons/Apps";
 import NotificationsIcon from "@material-ui/icons/Notifications";
 import Avatar from "@material-ui/core/Avatar";
 import { Link } from "react-router-dom";
+import HomeIcon from "@material-ui/icons/Home";
+import WhatshotIcon from "@material-ui/icons/Whatshot";
+import SubscriptionsIcon from "@material-ui/icons/Subscriptions";
+import VideoLibraryIcon from "@material-ui/icons/VideoLibrary";
+
 export default function Header() {
   const [inputSearch, setInputSearch] = useState("");
   return (
@@ -14,10 +19,10 @@ export default function Header() {
       <div className="header_left">
         <MenuIcon />
         <Link to="/">
-        <img
-          className="header_logo"
-          src="https://upload.wikimedia.org/wikipedia/commons/e/e1/Logo_of_YouTube_%282015-2017%29.svg"
-          alt="YTlogo"
+          <img
+            className="header_logo"
+            src="https://upload.wikimedia.org/wikipedia/commons/e/e1/Logo_of_YouTube_%282015-2017%29.svg"
+            alt="YTlogo"
           />
         </Link>
       </div>
@@ -31,7 +36,7 @@ export default function Header() {
         />
 
         <Link to={`/search/${inputSearch}`}>
-        <SearchIcon className="header_inputButton" />
+          <SearchIcon className="header_inputButton" />
         </Link>
       </div>
 
@@ -43,6 +48,25 @@ export default function Header() {
           alt="AJ"
           src="https://media-exp1.licdn.com/dms/image/C5603AQG-OcAZC-Apsw/profile-displayphoto-shrink_200_200/0/1641126144836?e=1647475200&v=beta&t=DyJbJECidtlksir3QdQMH6z_sUUk691u_4Ya9JHWoJU"
         />
+      </div>
+
+      <div className="footer">
+        <div className="footer_icons">
+          <HomeIcon />
+          <p>Home</p>
+        </div>
+        <div className="footer_icons">
+          <WhatshotIcon />
+          <p>Trending</p>
+        </div>
+        <div className="footer_icons">
+          <SubscriptionsIcon />
+          <p>Subscribtions</p>
+        </div>
+        <div className="footer_icons">
+          <VideoLibraryIcon />
+          <p>Library</p>
+        </div>
       </div>
     </div>
   );
--- src/MyComponents/RecommendedVideos.js
+++ src/MyComponents/RecommendedVideos.js
@@ -1,11 +1,26 @@
 import React from "react";
 import "../RecommendedVideos.css";
 import VideoCard from "./VideoCard";
+import { Link } from "react-router-dom";
+import Avatar from "@material-ui/core/Avatar";
+
 export default function RecommendedVideos() {
   return (
     <div className="recommendedVideos">
-      <h2>Recommended</h2>
       <div className="recommendedVideos_videos">
+        <div className="heading">
+          <Link to="/">
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/e/e1/Logo_of_YouTube_%282015-2017%29.svg"
+              alt="YTlogo"
+              height={"25px"}
+            />
+          </Link>
+          <Avatar
+            alt="AJ"
+            src="https://media-exp1.licdn.com/dms/image/C5603AQG-OcAZC-Apsw/profile-displayphoto-shrink_200_200/0/1641126144836?e=1647475200&v=beta&t=DyJbJECidtlksir3QdQMH6z_sUUk691u_4Ya9JHWoJU"
+          />
+        </div>
         <VideoCard
           title="| Basics of C (Part 1) | C-Programming - By W.INTELLI |"
           views="2.3M Views"
